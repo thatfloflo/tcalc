b5581f6fa21661fe1ace549adffb206f990ca54a
Florian Breit <florian@florian.me.uk>
2025-12-24 17:27:13 +0100

    Implement conversions for Value and Bitseq

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,8 @@ use lazy_static::lazy_static;
 use malachite::base::num::basic::traits::Zero;
 use malachite::base::num::conversion::string::options::FromSciStringOptions;
 use malachite::base::num::conversion::traits::{FromSciString, FromStringBase};
-use malachite::{Integer, Rational};
+use malachite::base::strings::ToBinaryString;
+use malachite::{Integer, Natural, Rational};
 use regex::Regex;
 use subenum::subenum;
 
@@ -66,6 +67,42 @@ enum TokenType {
     ImplicitMultiplication,
 }
 
+impl Display for TokenType {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "{}",
+            match self {
+                Self::Expression => "Expression",
+                Self::Bitseq => "Bitseq",
+                Self::Integer => "Integer",
+                Self::Rational => "Rational",
+                Self::AmbiguousOperator => "AmbiguousOperator",
+                Self::UnaryOperator => "UnaryOperator",
+                Self::BinaryOperator => "BinaryOperator",
+                Self::VariableIdentifier => "VariableIdentifier",
+                Self::UnaryFunctionIdentifier => "UnaryFunctionIdentifier",
+                Self::BinaryFunctionIdentifier => "BinaryFunctionIdentifier",
+                Self::ImplicitMultiplication => "ImplicitMultiplication",
+            }
+        )
+    }
+}
+
+impl Display for ValueType {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "{}",
+            match self {
+                Self::Bitseq => "Bitseq",
+                Self::Integer => "Integer",
+                Self::Rational => "Rational",
+            }
+        )
+    }
+}
+
 #[derive(Clone, Copy, Debug)]
 struct Bitseq {
     value: u128,
@@ -92,8 +129,76 @@ impl Bitseq {
         if s.len() < 1 || s.len() > 128 || s.chars().any(|c| !(&['0', '1'].contains(&c))) {
             None
         } else {
-            let value = if let Some(v) = u128::from_string_base(2, s) { v } else { return None ; };
-            Some(Bitseq { value, len: s.len() })
+            let value = if let Some(v) = u128::from_string_base(2, s) {
+                v
+            } else {
+                return None;
+            };
+            Some(Bitseq {
+                value,
+                len: s.len(),
+            })
+        }
+    }
+}
+
+impl Into<Integer> for Bitseq {
+    fn into(self) -> Integer {
+        Integer::from(self.value)
+    }
+}
+
+impl Into<Rational> for Bitseq {
+    fn into(self) -> Rational {
+        Rational::from(self.value)
+    }
+}
+
+impl TryFrom<Integer> for Bitseq {
+    type Error = ConversionError;
+
+    fn try_from(value: Integer) -> Result<Self, Self::Error> {
+        if value < 0 {
+            Err(ConversionError {
+                msg: "Cannot convert negative Integer to Bitseq".to_string(),
+            })
+        } else {
+            let natural = match Natural::try_from(value) {
+                Ok(v) => v,
+                Err(_) => {
+                    return Err(ConversionError {
+                        msg: "Cannot convert non-natural Integer to Bitseq".to_string(),
+                    });
+                }
+            };
+            // There's no direct (Try)From/Into Trait for Naturals and primitive unsigned types,
+            // so this isn't ideal but it works
+            let bitstr = natural.to_binary_string();
+            match Self::from_str(&bitstr) {
+                Some(v) => Ok(v),
+                None => Err(ConversionError {
+                    msg: "Cannot convert Integer that's too wide for u128 to Bitseq".to_string(),
+                }),
+            }
+        }
+    }
+}
+
+impl TryFrom<Rational> for Bitseq {
+    type Error = ConversionError;
+
+    fn try_from(value: Rational) -> Result<Self, Self::Error> {
+        match Integer::try_from(value) {
+            Ok(v) => match Self::try_from(v) {
+                Ok(b) => Ok(b),
+                Err(e) => Err(ConversionError {
+                    msg: e.msg.replace("Integer", "Rational"),
+                }),
+            },
+            Err(_) => Err(ConversionError {
+                msg: "Cannot convert Rational to Bitseq that doesn't losslessly convert to Integer"
+                    .to_string(),
+            }),
         }
     }
 }
@@ -140,17 +245,17 @@ impl Value {
 
     fn _strip_str(s: &str) -> String {
         let result = s.replace('_', "").replace(',', ".");
-        if Value::_has_base_prefix(s) {
-            Value::_strip_base_prefix(result)
+        if Self::_has_base_prefix(s) {
+            Self::_strip_base_prefix(result)
         } else {
             result
         }
     }
 
     fn _from_bitseq_str(s: &str, position: Position) -> Result<Value, ParseError> {
-        let norm_s = Value::_strip_str(s);
+        let norm_s = Self::_strip_str(s);
         match Bitseq::from_str(&norm_s) {
-            Some(b) => Ok(Value::from_bitseq(b)),
+            Some(b) => Ok(Self::from_bitseq(b)),
             None => Err(ParseError {
                 msg: format!(
                     "Failed to parse string \"{}\" (normalised to \"{}\" into bit-sequence value",
@@ -161,10 +266,10 @@ impl Value {
         }
     }
 
-    fn _from_int_str(s: &str, base: u8, position: Position) -> Result<Value, ParseError> {
-        let norm_s = Value::_strip_str(s);
+    fn _from_int_str(s: &str, base: u8, position: Position) -> Result<Self, ParseError> {
+        let norm_s = Self::_strip_str(s);
         match Integer::from_string_base(base, &norm_s) {
-            Some(i) => Ok(Value::from_integer(i)),
+            Some(i) => Ok(Self::from_integer(i)),
             None => Err(ParseError {
                 msg: format!(
                     "Failed to parse string \"{}\" (normalised to \"{}\" into integer value",
@@ -175,12 +280,12 @@ impl Value {
         }
     }
 
-    fn _from_frac_str(s: &str, base: u8, position: Position) -> Result<Value, ParseError> {
-        let norm_s = Value::_strip_str(s);
+    fn _from_frac_str(s: &str, base: u8, position: Position) -> Result<Self, ParseError> {
+        let norm_s = Self::_strip_str(s);
         let mut options = FromSciStringOptions::default();
         options.set_base(base);
         match Rational::from_sci_string_with_options(&norm_s, options) {
-            Some(r) => Ok(Value::from_rational(r)),
+            Some(r) => Ok(Self::from_rational(r)),
             None => Err(ParseError {
                 msg: format!(
                     "Failed to parse string \"{}\" (normalised to \"{}\" into rational value",
@@ -191,8 +296,8 @@ impl Value {
         }
     }
 
-    fn from_str(s: &str, position: Position) -> Result<Value, ParseError> {
-        let base: u8 = if let Some(b) = Value::_check_str_and_get_base(s) {
+    fn from_str(s: &str, position: Position) -> Result<Self, ParseError> {
+        let base: u8 = if let Some(b) = Self::_check_str_and_get_base(s) {
             b
         } else {
             return Err(ParseError {
@@ -200,17 +305,17 @@ impl Value {
                 position: position,
             });
         };
-        if Value::_has_fractional_separator(s) {
-            Value::_from_frac_str(s, base, position)
+        if Self::_has_fractional_separator(s) {
+            Self::_from_frac_str(s, base, position)
         } else if base == 2 {
             Value::_from_bitseq_str(s, position)
         } else {
-            Value::_from_int_str(s, base, position)
+            Self::_from_int_str(s, base, position)
         }
     }
 
-    fn from_integer(i: Integer) -> Value {
-        Value {
+    fn from_integer(i: Integer) -> Self {
+        Self {
             type_: ValueType::Integer,
             val_integer: i,
             val_rational: Rational::ZERO,
@@ -218,8 +323,8 @@ impl Value {
         }
     }
 
-    fn from_rational(r: Rational) -> Value {
-        Value {
+    fn from_rational(r: Rational) -> Self {
+        Self {
             type_: ValueType::Rational,
             val_integer: Integer::ZERO,
             val_rational: r,
@@ -227,14 +332,152 @@ impl Value {
         }
     }
 
-    fn from_bitseq(b: Bitseq) -> Value {
-        Value {
+    fn from_bitseq(b: Bitseq) -> Self {
+        Self {
             type_: ValueType::Bitseq,
             val_integer: Integer::ZERO,
             val_rational: Rational::ZERO,
-            val_bitseq: b
+            val_bitseq: b,
         }
     }
+
+    fn try_mutate_into(&mut self, into_type: ValueType) -> Result<(), ConversionError> {
+        if into_type == self.type_ {
+            return Ok(());
+        }
+        if self.type_ == ValueType::Bitseq {
+            if into_type == ValueType::Integer {
+                self.val_integer = self.val_bitseq.into();
+                self.val_bitseq = Bitseq::ZERO;
+                return Ok(());
+            }
+            if into_type == ValueType::Rational {
+                self.val_rational = self.val_bitseq.into();
+                self.val_bitseq = Bitseq::ZERO;
+                return Ok(());
+            }
+        }
+        if self.type_ == ValueType::Integer {
+            if into_type == ValueType::Bitseq {
+                match Bitseq::try_from(self.val_integer.clone()) {
+                    Ok(v) => {
+                        self.val_bitseq = v;
+                        self.val_integer = Integer::ZERO;
+                        return Ok(());
+                    }
+                    Err(e) => return Err(e),
+                }
+            }
+            if into_type == ValueType::Rational {
+                self.val_rational = Rational::from(self.val_integer.clone());
+                self.val_integer = Integer::ZERO;
+                return Ok(());
+            }
+        }
+        if self.type_ == ValueType::Rational {
+            if into_type == ValueType::Bitseq {
+                match Bitseq::try_from(self.val_rational.clone()) {
+                    Ok(v) => {
+                        self.val_bitseq = v;
+                        self.val_rational = Rational::ZERO;
+                        return Ok(());
+                    }
+                    Err(e) => return Err(e),
+                }
+            }
+            if into_type == ValueType::Integer {
+                match Integer::try_from(self.val_rational.clone()) {
+                    Ok(v) => {
+                        self.val_integer = v;
+                        self.val_rational = Rational::ZERO;
+                        return Ok(());
+                    }
+                    Err(_) => {
+                        return Err(ConversionError {
+                            msg: "Rational could not be losslessly converted to Integer"
+                                .to_string(),
+                        });
+                    }
+                }
+            }
+        }
+        Err(ConversionError {
+            msg: format!(
+                "No known conversion path to mutate {} to {}",
+                self.type_, into_type
+            ),
+        })
+    }
+}
+
+impl From<Rational> for Value {
+    fn from(item: Rational) -> Self {
+        Self::from_rational(item)
+    }
+}
+
+impl From<Integer> for Value {
+    fn from(item: Integer) -> Self {
+        Self::from_integer(item)
+    }
+}
+
+impl From<Bitseq> for Value {
+    fn from(item: Bitseq) -> Self {
+        Self::from_bitseq(item)
+    }
+}
+
+impl TryFrom<&str> for Value {
+    type Error = ParseError;
+
+    fn try_from(item: &str) -> Result<Self, Self::Error> {
+        Self::from_str(item, Position { line: 0, chr: 0 })
+    }
+}
+
+impl Into<Rational> for Value {
+    fn into(self) -> Rational {
+        match self.type_ {
+            ValueType::Bitseq => self.val_bitseq.into(),
+            ValueType::Integer => self.val_integer.into(),
+            ValueType::Rational => self.val_rational,
+        }
+    }
+}
+
+impl TryInto<Integer> for Value {
+    type Error = ConversionError;
+
+    fn try_into(self) -> Result<Integer, Self::Error> {
+        match self.type_ {
+            ValueType::Bitseq => Ok(self.val_bitseq.into()),
+            ValueType::Integer => Ok(self.val_integer),
+            ValueType::Rational => match self.val_rational.try_into() {
+                Ok(v) => Ok(v),
+                Err(_) => Err(ConversionError {
+                    msg: "Cannot convert Rational with denominator > 1 to Integer losslessly"
+                        .to_string(),
+                }),
+            },
+        }
+    }
+}
+
+impl TryInto<Bitseq> for Value {
+    type Error = ConversionError;
+
+    fn try_into(self) -> Result<Bitseq, Self::Error> {
+        match self.type_ {
+            ValueType::Bitseq => Ok(self.val_bitseq),
+            ValueType::Integer => Bitseq::try_from(self.val_integer),
+            ValueType::Rational => Bitseq::try_from(self.val_rational),
+        }
+    }
+}
+
+struct ConversionError {
+    msg: String,
 }
 
 impl Display for Value {
